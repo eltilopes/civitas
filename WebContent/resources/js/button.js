3bd825c469b900f0198f3e29a6e882cb8d00d2ca
elton <elton@192.168.2.6>
2016-05-24 15:00:42 -0300

    ALTERACAO LAYOUT #1

---
+++ WebContent/resources/js/button.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+	var element, ink, d, x, y;
+	$(".btn").click(function(e){
+		element = $(this);
+		
+		if(element.find(".ink").length == 0)
+			element.prepend("<span class='ink'></span>");
+			
+		ink = element.find(".ink");
+		
+		ink.removeClass("animate");
+		
+
+		if(!ink.height() && !ink.width())
+		{
+			d = Math.max(element.outerWidth(), element.outerHeight());
+			ink.css({height: d, width: d});
+		}
+		
+		x = e.pageX - element.offset().left - ink.width()/2;
+		y = e.pageY - element.offset().top - ink.height()/2;
+		
+		ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+	})
+})
+	
